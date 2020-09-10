8b64e16c67a725e4a44bec25100661128be91e06
coco99166 imac <coco99166@outlook.com>
2020-09-10 12:05:42 -0700

    completes text component and contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 
 class App extends React.Component {
@@ -46,11 +48,11 @@ class App extends React.Component {
                 <Link className="nav-link" to="contact">Contact</Link>
               </Nav>
 
-              <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>}/>
-
             </Navbar.Collapse>
           </Navbar>
-
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text}/>}/>
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
         </Container>
       </Router>
     );
